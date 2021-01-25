0e7da6b91ad61565d77be275e39a590bf27b5f1a
howardg123 <60571748+howardg123@users.noreply.github.com>
2021-01-25 22:02:12 +0800

    combination field and exp continuation

--- src/model/Dec64.java
+++ src/model/Dec64.java
@@ -3,8 +3,8 @@ package model;
 import java.io.Console;
 
 public class Dec64 {
-    
-    //Base-10 representation of the decimal value
+
+    // Base-10 representation of the decimal value
     private String base10;
     private Integer exponent;
     private String binaryBase10;
@@ -19,10 +19,10 @@ public class Dec64 {
 
     public void setBase10(String n) {
         char sign = n.charAt(0);
-        if(sign == '-') {
+        if (sign == '-') {
             signBit = "1";
             n = n.substring(1);
-        }else {
+        } else {
             signBit = "0";
         }
 
@@ -33,23 +33,23 @@ public class Dec64 {
         return base10;
     }
 
+    public Integer getExponent() {
+        return exponent;
+    }
+
     public void setExponent(Integer n) {
         this.exponent = n;
     }
 
-    public Integer getExponent() {
-        return exponent;
-    }
-    
     public void fixInput() {
         String temp = base10;
         int pos = temp.indexOf('.');
 
-        if(pos >= 0 && Integer.parseInt(temp.substring(pos + 1, temp.length())) > 0) {
+        if (pos >= 0 && Integer.parseInt(temp.substring(pos + 1, temp.length())) > 0) {
             exponent = exponent - temp.substring(pos + 1, temp.length()).length();
             temp = temp.substring(0, pos) + temp.substring(pos + 1, temp.length());
         }
-         
+
         base10 = temp;
         normalize();
     }
@@ -57,17 +57,26 @@ public class Dec64 {
     private void normalize() {
         String temp = base10;
 
-        for(int i = temp.length(); i < 16; i++ ) {
+        for (int i = temp.length(); i < 16; i++) {
             temp = "0" + temp;
         }
 
         int mostSignificant = Integer.valueOf(temp.substring(0, 1));
         System.out.println("MostSig: " + mostSignificant + " binary: " + binaryConvert(mostSignificant));
 
-        //-> mostSig to bcd
-    
+        // -> mostSig to bcd
+
         String exponentRep = binaryConvert(exponent + 398);
 
+        for (int i = exponentRep.length(); i < 10; i++) {
+            exponentRep = "0" + exponentRep;
+        }
+
+        String combiFieldValue = combiField(mostSignificant, exponentRep);
+
+        String ExpContValue = getExpContinuation(exponentRep);
+
+        System.out.println("Combination field: " + combiFieldValue + "\nExponent Continuation: " + ExpContValue);
 
     }
 
@@ -75,9 +84,9 @@ public class Dec64 {
         String temp = "";
 
         do {
-            temp = String.valueOf(n%2) + temp;  
+            temp = String.valueOf(n % 2) + temp;
             n /= 2;
-        }while(n > 0);
+        } while (n > 0);
         return String.valueOf(temp);
     }
 
@@ -85,13 +94,33 @@ public class Dec64 {
         return "";
     }
 
-    private String combiField(/**most significant and exponent rep pass**/) {
-        return "";
+    private String combiField(int mostSignificant, String exponentRep) {
+
+        String binaryExpoRep = binaryConvert(mostSignificant);
+        String expMSB = exponentRep.substring(0, 2);
+        String msbSubstring = "";
+        String combination = "";
+        for (int i = binaryExpoRep.length(); i < 4; i++) {
+            binaryExpoRep = "0" + binaryExpoRep;
+        }
+
+        if (mostSignificant >= 0 && mostSignificant <= 7) {
+            msbSubstring = binaryExpoRep.substring(1);
+            combination = expMSB.concat(msbSubstring);
+
+        } else if (mostSignificant >= 8 && mostSignificant <= 9) {
+            msbSubstring = binaryExpoRep.substring(3);
+            combination = combination.concat("11");
+            combination = combination.concat(expMSB);
+            combination = combination.concat(msbSubstring);
+        }
+
+        return combination;
     }
 
-    //exponent continuation
-    private String getExpContinuation() {
-        return "";
+    // exponent continuation
+    private String getExpContinuation(String exponentRep) {
+        return exponentRep.substring(2);
     }
 
     private String getCoefContinuation() {
