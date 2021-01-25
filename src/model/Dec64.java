9fdafd20bda23a62380dd4f40bec6b68a1ba34de
simpleuser11 <52992041+simpleuser11@users.noreply.github.com>
2021-01-26 03:53:31 +0800

    Update Dec64.java

--- src/model/Dec64.java
+++ src/model/Dec64.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.io.Console;
+import java.util.Arrays;
 
 public class Dec64 {
 
@@ -77,7 +78,6 @@ public class Dec64 {
         String ExpContValue = getExpContinuation(exponentRep);
 
         System.out.println("Combination field: " + combiFieldValue + "\nExponent Continuation: " + ExpContValue);
-
     }
 
     private String binaryConvert(int n) {
@@ -90,8 +90,175 @@ public class Dec64 {
         return String.valueOf(temp);
     }
 
-    private String bcd(String bin) {
+    private String bcd(int num) {
+        switch(num){
+            case 0: return "0000"; 
+            case 1: return "0001";
+            case 2: return "0010";
+            case 3: return "0011";
+            case 4: return "0100";
+            case 5: return "0101";
+            case 6: return "0110";
+            case 7: return "0111";
+            case 8: return "1000";
+            case 9: return "1001";
+        }
         return "";
+    }
+
+    private String DBCD(int num){
+        int hundreds, tens, ones;
+        String hdString, tnString, onString, numString;
+        char[] dbcdString = new char[11];
+        char a, e, i;
+
+        hundreds =  num/100;
+        num =  num - hundreds * 100;
+        tens = num/10;
+        ones = num - tens *10;
+
+        hdString = this.bcd(hundreds);
+        tnString = this.bcd(tens);
+        onString = this.bcd(ones);
+
+        numString = hdString  + tnString + onString;
+
+        a = numString.charAt(0);
+        e = numString.charAt(4);
+        i = numString.charAt(8);
+        switch(a){
+            case '0': 
+                switch(e){
+                    case '0':
+                        switch(i){
+                            case '0': 
+                            dbcdString[0] = numString.charAt(1); 
+                            dbcdString[1] = numString.charAt(2);
+                            dbcdString[2] = numString.charAt(3);
+                            dbcdString[3] = numString.charAt(5); 
+                            dbcdString[4] = numString.charAt(6); 
+                            dbcdString[5] = numString.charAt(7); 
+                            dbcdString[6] = '0';
+                            dbcdString[7] = numString.charAt(9);
+                            dbcdString[8] = numString.charAt(10);
+                            dbcdString[9] = numString.charAt(11);
+                            dbcdString[10] = '\0';
+                            break;
+
+                            case '1':
+                            dbcdString[0] = numString.charAt(1); 
+                            dbcdString[1] = numString.charAt(2);
+                            dbcdString[2] = numString.charAt(3);
+                            dbcdString[3] = numString.charAt(5); 
+                            dbcdString[4] = numString.charAt(6); 
+                            dbcdString[5] = numString.charAt(7); 
+                            dbcdString[6] = '1';
+                            dbcdString[7] = '0';
+                            dbcdString[8] = '0';
+                            dbcdString[9] = numString.charAt(11);
+                            dbcdString[10] = '\0';
+                            break;                            
+                        };
+                        break;
+                        
+                    case '1': 
+                        switch(i){
+                            case '0': 
+                            dbcdString[0] = numString.charAt(1); 
+                            dbcdString[1] = numString.charAt(2);
+                            dbcdString[2] = numString.charAt(3);
+                            dbcdString[3] = numString.charAt(9); 
+                            dbcdString[4] = numString.charAt(10); 
+                            dbcdString[5] = numString.charAt(7); 
+                            dbcdString[6] = '1';
+                            dbcdString[7] = '0';
+                            dbcdString[8] = '1';
+                            dbcdString[9] = numString.charAt(11);
+                            dbcdString[10] = '\0';
+                            break;
+
+                            case '1':
+                            dbcdString[0] = numString.charAt(1); 
+                            dbcdString[1] = numString.charAt(2);
+                            dbcdString[2] = numString.charAt(3);
+                            dbcdString[3] = '1';
+                            dbcdString[4] = '0';
+                            dbcdString[5] = numString.charAt(7); 
+                            dbcdString[6] ='1';
+                            dbcdString[7] = '1';
+                            dbcdString[8] = '1';
+                            dbcdString[9] = numString.charAt(11);
+                            dbcdString[10] = '\0';
+                            break;                            
+                        };
+                        break;
+                }; break;
+            case 1: 
+                switch(e){
+                    case 0:
+                        switch(i){
+                            case 0:
+                            dbcdString[0] = numString.charAt(9); 
+                            dbcdString[1] = numString.charAt(10);
+                            dbcdString[2] = numString.charAt(3);
+                            dbcdString[3] = numString.charAt(5); 
+                            dbcdString[4] = numString.charAt(6); 
+                            dbcdString[5] = numString.charAt(7); 
+                            dbcdString[6] = '1';
+                            dbcdString[7] = '1';
+                            dbcdString[8] = '0';
+                            dbcdString[9] = numString.charAt(11);
+                            dbcdString[10] = '\0';
+                            break;
+                            case 1:
+                            dbcdString[0] = numString.charAt(5); 
+                            dbcdString[1] = numString.charAt(6);
+                            dbcdString[2] = numString.charAt(3);
+                            dbcdString[3] = '0';
+                            dbcdString[4] = '1';
+                            dbcdString[5] = numString.charAt(7); 
+                            dbcdString[6] = '1';
+                            dbcdString[7] = '1';
+                            dbcdString[8] = '1';
+                            dbcdString[9] = numString.charAt(11);
+                            dbcdString[10] = '\0';
+                            break;
+                        };
+                    case 1: 
+                        switch(i){
+                            case 0: 
+                            dbcdString[0] = numString.charAt(9); 
+                            dbcdString[1] = numString.charAt(10);
+                            dbcdString[2] = numString.charAt(3);
+                            dbcdString[3] = '0';
+                            dbcdString[4] = '0';
+                            dbcdString[5] = numString.charAt(7); 
+                            dbcdString[6] = '1';
+                            dbcdString[7] = '1';
+                            dbcdString[8] = '1';
+                            dbcdString[9] = numString.charAt(11);
+                            dbcdString[10] = '\0';
+                            break;
+
+                            case 1:
+                            dbcdString[0] = '0';
+                            dbcdString[1] = '0';
+                            dbcdString[2] = numString.charAt(3);
+                            dbcdString[3] = '1';
+                            dbcdString[4] = '1';
+                            dbcdString[5] = numString.charAt(7); 
+                            dbcdString[6] = '1';
+                            dbcdString[7] = '1';
+                            dbcdString[8] = '1';
+                            dbcdString[9] = numString.charAt(11);
+                            dbcdString[10] = '\0';
+                            break;
+                        };
+                    break;
+                }; break;
+        }
+        
+        return Arrays.toString(dbcdString);
     }
 
     private String combiField(int mostSignificant, String exponentRep) {
